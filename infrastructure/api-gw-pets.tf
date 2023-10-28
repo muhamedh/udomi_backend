91a5c54c70d8f5f67fdb83e6a8ddd3152ebd1dbb
muhamedh <muhammed.hamzic@gmail.com>
2023-10-28 22:58:31 +0200

    add users POST method

--- infrastructure/api-gw-pets.tf
+++ infrastructure/api-gw-pets.tf
@@ -1,3 +1,4 @@
+# ROOT API "/"
 resource "aws_api_gateway_rest_api" "pets_api" {
   name = "dev1-pets"
   endpoint_configuration {
@@ -5,34 +6,89 @@ resource "aws_api_gateway_rest_api" "pets_api" {
   }
 }
 
+# set authorizer for whole API resource
+resource "aws_api_gateway_authorizer" "api_authorizer" {
+  name          = "CognitoUserPoolAuthorizer"
+  type          = "COGNITO_USER_POOLS"
+  rest_api_id   = aws_api_gateway_rest_api.pets_api.id
+  provider_arns = [aws_cognito_user_pool.user_pool.arn]
+}
+
+# ALL /pets HTTP methods go under this resource
 resource "aws_api_gateway_resource" "pets_endpoint" {
-  parent_id   = aws_api_gateway_rest_api.pets_api.root_resource_id
   path_part   = "pets"
+  parent_id   = aws_api_gateway_rest_api.pets_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.pets_api.id
 }
 
-resource "aws_api_gateway_authorizer" "api_authorizer" {
-    name = "CognitoUserPoolAuthorizer"
-    type = "COGNITO_USER_POOLS"
-    rest_api_id = aws_api_gateway_rest_api.pets_api.id
-    provider_arns = [ aws_cognito_user_pool.user_pool.arn ]
-}
+# create a POST method under /pets resource
 resource "aws_api_gateway_method" "add_pets_method" {
   authorization = "COGNITO_USER_POOLS"
-  #authorizer_id = aws_cognito_user_pool.user_pool.id
   authorizer_id = aws_api_gateway_authorizer.api_authorizer.id
   http_method   = "POST"
   resource_id   = aws_api_gateway_resource.pets_endpoint.id
   rest_api_id   = aws_api_gateway_rest_api.pets_api.id
 }
 
+# create a LAMBDA proxy for the POST /pets resource
 resource "aws_api_gateway_integration" "lambda_pets_api_integration" {
-  http_method = aws_api_gateway_method.add_pets_method.http_method
-  resource_id = aws_api_gateway_resource.pets_endpoint.id
-  rest_api_id = aws_api_gateway_rest_api.pets_api.id
-  uri = aws_lambda_function.pets_api.invoke_arn
-  type        = "AWS_PROXY"
+  http_method             = aws_api_gateway_method.add_pets_method.http_method
+  resource_id             = aws_api_gateway_resource.pets_endpoint.id
+  rest_api_id             = aws_api_gateway_rest_api.pets_api.id
+  uri                     = aws_lambda_function.pets_api.invoke_arn
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
+}
+
+#create a OPTIONS method for CORS needs
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.pets_api.id
+  resource_id   = aws_api_gateway_resource.pets_endpoint.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# hardcode OPTIONS response 
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.pets_api.id
+  resource_id = aws_api_gateway_resource.pets_endpoint.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+  depends_on = [aws_api_gateway_method.options_method]
+}
+
+# mock an integration in order for OPTIONS method to return CORS info
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.pets_api.id
+  resource_id = aws_api_gateway_resource.pets_endpoint.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{statusCode: 200}"
+  }
+  depends_on = [aws_api_gateway_method.options_method]
+}
+
+# integration response -> what the CORS policy for this method will be.
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.pets_api.id
+  resource_id = aws_api_gateway_resource.pets_endpoint.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = [aws_api_gateway_method_response.options_200]
 }
 
 resource "aws_api_gateway_deployment" "pets_deployment" {
@@ -50,9 +106,12 @@ resource "aws_api_gateway_deployment" "pets_deployment" {
       aws_api_gateway_resource.pets_endpoint.id,
       aws_api_gateway_method.add_pets_method.id,
       aws_api_gateway_integration.lambda_pets_api_integration.id,
+      aws_api_gateway_method.edit_user_endpoint.id,
+      aws_api_gateway_integration.lambda_user_pets_api_integration.id,
+      aws_api_gateway_authorizer.api_authorizer.id,
+      aws_api_gateway_method.options_method.id
     ]))
   }
-
   lifecycle {
     create_before_destroy = true
   }
--- infrastructure/apis/pets/src/handler.ts
+++ infrastructure/apis/pets/src/handler.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { v4 as uuidv4 } from "uuid";
 import { APIGatewayProxyEvent } from "aws-lambda";
@@ -37,37 +37,91 @@ const addPet = async (userId: any, pet: any) => {
     throw Error("Database insert failed");
   }
 };
+const getMyPets = async (userId: any) => {
+  console.log("Proceeding with my pet fetch");
+  const command = new QueryCommand({
+    TableName: process.env.USER_TABLE,
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": userId,
+    },
+  });
+  try {
+    const data = await client.send(command);
+    return data.Items;
+  } catch (e) {
+    console.log(`Database fetch failed with message ${e}`);
+    throw Error("Database fetch failed");
+  }
+}
+const editUser = async (userId: any, user: any) => {
+  console.log("Proceeding with user insert");
+  const command = new PutCommand({
+    TableName: process.env.USER_TABLE,
+    Item: {
+      id: userId,
+      pet_id: "PET_ID#",
+      data: user ? JSON.stringify(user) : {},
+    },
+  });
+  try {
+    await docClient.send(command);
+  } catch (e) {
+    console.log(`Database insert failed with message ${e}`);
+    throw Error("Database insert failed");
+  }
+};
 export const handler = async (event: APIGatewayProxyEvent, context: any) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
+  let userId = null;
+  if (event?.headers?.Authorization) {
+    userId = getUserId(event?.headers?.Authorization);
+  }
   try {
-    if (
-      event?.httpMethod === "POST" &&
-      event?.resource === "/pets" &&
-      event?.headers?.Authorization
-    ) {
-      const userId = getUserId(event?.headers?.Authorization);
+    if (event?.httpMethod === "POST" && event?.resource === "/pets") {
       await addPet(userId, event?.body);
+    } else if(event?.httpMethod === "GET" && event?.resource === "/pets/mine") {
+      const myPets = await getMyPets(userId);
+      return {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(myPets)
+      };
+    }else if (event?.httpMethod === "POST" && event?.resource === "/users") {
+      await editUser(userId, event?.body);
     } else {
       return {
         isBase64Encoded: false,
         statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
         body: JSON.stringify({
           message: "Invalidna metoda.",
         }),
       };
     }
-
     return {
       isBase64Encoded: false,
       statusCode: 200,
-      body: JSON.stringify({
-        message: "Ljubimac uspješno dodan.",
-      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   } catch (error) {
     return {
       isBase64Encoded: false,
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
       body: JSON.stringify({
         message: "Greška na serveru.",
       }),
